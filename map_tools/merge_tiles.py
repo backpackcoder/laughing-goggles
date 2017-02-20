33d6b69bd011945cb13ef39123d9aeb87554a89d
Andrew Hammer <backpackcoder@users.noreply.github.com>
2017-02-21 00:03:10 +0300

    Maint: File cleanup

--- map_tools/merge_tiles.py
+++ map_tools/merge_tiles.py
@@ -2,45 +2,46 @@ from PIL import Image
 import os
 import shutil
 
-"""
-Walks a python directory structure for a collection of
-source sirectories and returns a list of files and whether
-to merge
-"""
+
 def collect_files(src):
-	file_list = dict()
-	for root in src:
-		for subdir, dirs, files in os.walk(root):
-			for f in files:
-				relpath = os.path.relpath(
-					os.path.join(subdir, f), root)
-				if relpath in file_list:
-					file_list[relpath].append(root)
-				else:
-					file_list[relpath] = [ root ]				
-	return file_list
+    """
+    Walks a python directory structure for a collection of
+    source sirectories and returns a list of files and whether
+    to merge
+    """
+    file_list = dict()
+    for root in src:
+        for subdir, dirs, files in os.walk(root):
+            for f in files:
+                relpath = os.path.relpath(
+                    os.path.join(subdir, f), root)
+                if relpath in file_list:
+                    file_list[relpath].append(root)
+                else:
+                    file_list[relpath] = [root]
+    return file_list
 
 
-"""
-Merge two directories merging images file if necessary
-"""
 def merge(src, dest):
-	paths = collect_files(src)
-	for f in paths:
-		f_dir =  os.path.join(dest, os.path.dirname(f))
-		if not os.path.exists(f_dir):
-			os.makedirs(f_dir)
-		if len(paths[f]) == 1:
-			srcFile = os.path.join(paths[f][0], f)
-			destFile = os.path.join(dest, f)
-			shutil.copyfile(srcFile, destFile)
-			print "copied {} to {}".format(srcFile, destFile)
-		else:
-			srcFile1 = os.path.join(paths[f][0], f)
-			srcFile2 = os.path.join(paths[f][1], f)
-			background = Image.open(srcFile1)
-			foreground = Image.open(srcFile2)
-			background.paste(foreground, (0, 0), foreground)
-			destFile = os.path.join(dest, f)
-			background.save(destFile, "PNG")
-			print "merged {} and {} to {}".format(srcFile1, srcFile2, destFile)
+    """
+    Merge two directories merging images file if necessary
+    """
+    paths = collect_files(src)
+    for f in paths:
+        f_dir = os.path.join(dest, os.path.dirname(f))
+        if not os.path.exists(f_dir):
+            os.makedirs(f_dir)
+        if len(paths[f]) == 1:
+            src_file = os.path.join(paths[f][0], f)
+            dest_file = os.path.join(dest, f)
+            shutil.copyfile(src_file, dest_file)
+            print "copied {} to {}".format(src_file, dest_file)
+        else:
+            src_file1 = os.path.join(paths[f][0], f)
+            src_file2 = os.path.join(paths[f][1], f)
+            background = Image.open(src_file1)
+            foreground = Image.open(src_file2)
+            background.paste(foreground, (0, 0), foreground)
+            dest_file = os.path.join(dest, f)
+            background.save(dest_file, "PNG")
+            print "merged {} and {} to {}".format(src_file1, src_file2, dest_file)
--- tests/test_merge_tiles.py
+++ tests/test_merge_tiles.py
@@ -4,43 +4,43 @@ from map_tools import merge_tiles
 from nose.tools import eq_, ok_
 
 test_data = {
-		"16/38876/31140.png" : [ "tests/data/src2" ],
-		"16/38877/31140.png" : [ "tests/data/src2" ],
-		"16/38877/31141.png" : [ "tests/data/src1" ],
-		"16/38878/31141.png" : [ "tests/data/src1" ],
-		"16/38878/31142.png" : [ "tests/data/src1" ],
-		"16/38877/31142.png" : [ "tests/data/src1", "tests/data/src2" ]
+        "16/38876/31140.png": ["tests/data/src2"],
+        "16/38877/31140.png": ["tests/data/src2"],
+        "16/38877/31141.png": ["tests/data/src1"],
+        "16/38878/31141.png": ["tests/data/src1"],
+        "16/38878/31142.png": ["tests/data/src1"],
+        "16/38877/31142.png": ["tests/data/src1", "tests/data/src2"]
 }
-expected_paths = [path for path in test_data ]
-src =  [ "tests/data/src1", "tests/data/src2" ]
+expected_paths = [path for path in test_data]
+src = ["tests/data/src1", "tests/data/src2"]
 dest_dir = "tests/data/out"
 
 
-"""
-Tests the collect_files function
-"""
 def test_collect_files():
-	src = ["tests/data/src1", "tests/data/src2"]
-	result = merge_tiles.collect_files(src)
-	actual_paths = [path for path in result ]
-	eq_(len(expected_paths), len(actual_paths))
+    """
+    Tests the collect_files function
+    """
+    result = merge_tiles.collect_files(src)
+    actual_paths = [path2 for path2 in result]
+    eq_(len(expected_paths), len(actual_paths))
 
-	for path in result:
-		ok_(path in test_data, "missing path {}".format(path))
-		eq_(len(test_data[path]), len(result[path]))
-		for root in result[path]:
-			ok_(root in test_data[path], 
-				"missing root {} in path {}".format(root, path))
+    for path3 in result:
+        ok_(path3 in test_data, "missing path {}".format(path3))
+        eq_(len(test_data[path3]), len(result[path3]))
+        for root in result[path3]:
+            ok_(root in test_data[path3],
+                "missing root {} in path {}".format(root, path3))
 
-"""
-Tests the merge_files function
-"""
+
 def test_merge():
-	if os.path.exists(dest_dir):
-		shutil.rmtree(dest_dir)
-	merge_tiles.merge(src, dest_dir)
-	files = []
-	for s, d, fs in os.walk(dest_dir):
-		for f in fs:
-			files.append(f)
-	eq_(len(expected_paths), len(files))
+    """
+    Tests the merge_files function
+    """
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    merge_tiles.merge(src, dest_dir)
+    files = []
+    for s, d, fs in os.walk(dest_dir):
+        for f in fs:
+            files.append(f)
+    eq_(len(expected_paths), len(files))
